296df2d2c1706c424276cf144374ebb8356b0329
cookiebanner2025 <analyticsmaster2@gmail.com>
2025-04-21 20:34:45 +0600

    Update cookie-consent.js

--- cookie-consent.js
+++ cookie-consent.js
@@ -69,7 +69,7 @@ const config = {
     uiTheme: 'default',
     
     // Banner position (new feature)
-    bannerPosition: 'left', // 'top', 'bottom', 'left', 'right'
+    bannerPosition: 'bottom', // 'top', 'bottom', 'left', 'right'
     
     // Separate controls for UI elements (new feature)
     uiControls: {
